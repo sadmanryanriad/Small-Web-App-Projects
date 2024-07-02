6b930802dd303e0e65071ed2f0fba2bc4a73e33c
sadmanryanriad <sadmanryanriad@gmail.com>
2024-07-02 14:00:36 +0600

    calculation and mapping

--- summation-table-react/src/table/Table.jsx
+++ summation-table-react/src/table/Table.jsx
@@ -1,35 +1,52 @@
+import { useState, useEffect } from "react";
+
+const fetchedData = {
+    'intern': 24000,
+    'savings': 20000,
+    "VU fee": 5000,
+    'mitu': 10000
+};
+
 const Table = () => {
-  return (
-    <div>
-      <table id="moneyTable" className="overflow-x-auto max-w-5xl m-auto">
-        <tr className="table_heading">
-          <th>Source</th>
-          <th>Amount</th>
-        </tr>
-        <tr>
-          <td>Intern</td>
-          <td>24,000</td>
-        </tr>
-        <tr>
-          <td>Savings</td>
-          <td>20,000</td>
-        </tr>
-        <tr>
-          <td>VU fees</td>
-          <td>5,000</td>
-        </tr>
-        <tr>
-          <td>Mitu</td>
-          <td>10,000</td>
-        </tr>
-        <tr className="border-2"></tr>
-        <tr className="total">
-          <td>Total</td>
-          <td id="totalAmount">sum</td>
-        </tr>
-      </table>
-    </div>
-  );
+    const [data] = useState(fetchedData);
+
+    useEffect(() => {
+        const calculateTotal = () => {
+            let total = Object.values(data).reduce((acc, currentValue) => acc + currentValue, 0);
+            // console.log(Object.values(data)); //returns an array of all the object values
+            return total.toLocaleString(); //toLocaleString adds comma after thousands
+        };
+        document.getElementById("totalAmount").innerText = calculateTotal();
+    }, [data]);
+
+    return (
+        <div>
+            <table id="moneyTable" className="overflow-x-auto max-w-5xl m-auto">
+                <thead className="table_heading">
+                    <tr>
+                        <th>Source</th>
+                        <th>Amount</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        Object.entries(data).map(([source,amount])=>{
+                            return <tr key={source}>
+                                <td>{source}</td>
+                                <td>{amount.toLocaleString()}</td>
+                            </tr>
+                        })
+                    }
+                </tbody>
+                <tfoot>
+                    <tr className="total">
+                        <td>Total</td>
+                        <td id="totalAmount">sum</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    );
 };
 
 export default Table;
